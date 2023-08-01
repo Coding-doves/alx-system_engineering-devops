90b6c2ca5b84af9199b575d17cbe4a6e7f8cfde9
Ada <obenedicta4@gmail.com>
2023-08-01 16:57:01 +0100

    regexp

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\[from:([a-zA-Z0-9+]+)\].\[to:([a-zA-Z0-9+]+)\].\[flags:([a-zA-Z0-9+]+)\]/).join(',')
+puts ARGV[0].scan(/\[from:([a-zA-Z0-9+]+)\].\[to:([a-zA-Z0-9+]+)\].\[flags:([10:-]+)\]/).join(',')

b84dac423b2d6b95d2d28b91586a85037d9f83c4
Ada <obenedicta4@gmail.com>
2023-10-02 20:40:16 +0100

    load balancing

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,19 @@
+# creating a custom HTTP header response
+# installing nginx
+package { 'nginx':
+  ensure => installed,
+}
+
+# creating a ninx confiuration file
+file { '/etc/nginx/conf.d/custom_header.conf':
+  ensure  => present,
+  content => 'add_header X-Served-By $hostname;',
+  notify  => Service['nginx'],
+}
+
+# run and enable nginx Service
+service { 'nginx':
+  ensure   => running,
+  enable   => true,
+  required => Package['nginx']
+}

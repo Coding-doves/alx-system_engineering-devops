e38f5e8bc7075751350eeafb83f1710d12bb6368
Ada <obenedicta4@gmail.com>
2023-10-04 13:54:38 +0100

    load balancer

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,19 +1,26 @@
 # creating a custom HTTP header response
-# installing nginx
+exec { 'apt-update':
+  command => '/usr/bin/apt-get -y update',
+  path    => ['/usr/bin', '/bin'],
+}
+# instailling nginx
 package { 'nginx':
   ensure => installed,
 }
 
 # creating a ninx confiuration file
-file { '/etc/nginx/conf.d/custom_header.conf':
-  ensure  => present,
-  content => 'add_header X-Served-By $hostname;',
-  notify  => Service['nginx'],
+file { '/var/www/html/index.html':
+  content => 'Hello World',
+}
+
+file_line { 'redirection-301':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-avaliable/default',
+  line   => "\tadd_header X-served-By ${hostname};",
+  after  => 'server_name_;',
 }
 
 # run and enable nginx Service
 service { 'nginx':
-  ensure   => running,
-  enable   => true,
-  require  => Package['nginx']
+  ensure => running,
 }

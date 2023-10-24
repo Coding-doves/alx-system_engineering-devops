9c4f800dfea943304b8cd6ed66775d73b8e0d5e3
Coding-doves <obenedicta4@gmail.com>
2023-10-24 05:47:41 +0100

    api

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -12,13 +12,13 @@ if __name__ == "__main__":
     users = requests.get(user).json()
     worker_tasks = "{}/todos".format(url)
     tasks = requests.get(worker_tasks).json()
-    emp_task = {use.get('id'): [{"username": username("username")
-                                "task": tas.get("title"),
-                                "completed": tas.get("completed"),
-                                for tas in tasks if tas.get('userId')
-                                == use.get('id')]
-                                for use in username
-                                }
+    emp_task = {use.get('id'): [{"username": username("username"),
+                                 "task": tas.get("title"),
+                                 "completed": tas.get("completed")}
+                                 for tas in tasks if tas.get('userId')
+                                 == use.get('id')]
+                                 for use in username
+                                 }
 
     with open("todo_all_employees.json", 'w', encoding="utf-8") as file:
         json.dump(emp_task, file)
